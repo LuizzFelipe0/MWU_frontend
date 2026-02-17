4521a10858322196ae4e4fc30e372737c90fdc13
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-17 13:04:50 -0300

    Update styles Footer with new margin and Copyright font-weight

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,23 +1,19 @@
 import styled from 'styled-components';
 
 export const Footer = styled.footer`
-  /* Layout */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100%;
 
-  /* Espaçamento consistente com o Header (2% nas laterais) */
   padding: 1.5rem 2%;
 
   background-color: #ffffff;
 
-  /* Borda superior para separar do conteúdo, igual à espessura do Header */
   border-top: 0.0625rem solid #c5c9c7;
 
-  /* Garante que o padding não quebre a largura total */
   box-sizing: border-box;
+  margin-top: auto; /* Garante que ele fique no final do flex */
 
   @media (min-width: 48rem) {
     flex-direction: column;
@@ -26,11 +22,10 @@ export const Footer = styled.footer`
 `;
 
 export const Copyright = styled.p`
-  /* Fonte menor que o título, mas ainda responsiva */
   font-size: clamp(0.875rem, 2.5vw, 1rem);
 
-  font-weight: 500;
-  color: #929090; /* Sua cor secundária */
+  font-weight: 450;
+  color: #929090;
   margin: 0.0625rem;
 
   font-family:
