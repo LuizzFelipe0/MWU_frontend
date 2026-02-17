b41e718080f48f2ca576d9aa71c3692064012ba9
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-17 13:00:00 -0300

    Update App.tsx to use MWU routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import * as S from './AppStyles';
-import CategoryTypesPage from './pages/CategoryTypes';
-import Header from './components/Header';
-import Footer from './components/Footer'; 
+
+import * as S from './globalStyles.ts';
+
+import MWURoutes from './routes.tsx';
 
 const App: React.FC = () => {
   return (
     <>
       <S.GlobalStyle />
-      <Header />
-      <S.AppContainer>
-        {/* Aqui renderizamos a página de tipos de categoria */}
-        <S.Card>
-          <CategoryTypesPage />
-        </S.Card>
-      </S.AppContainer>
-      <Footer />
+      <MWURoutes />
     </>
   );
 };
