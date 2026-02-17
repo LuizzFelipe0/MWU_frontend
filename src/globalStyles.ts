10fcff3e17deff84244891c2b6845459c91b8e7c
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-17 13:01:29 -0300

    Add margin: 0 at globalStyles

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    margin: 0;
+  }
+  
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
     background-color: #f2f2f7;
