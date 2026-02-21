fa70fb8d1edda16a7f1ed8e4c48f939fc73f4213
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-21 18:00:42 -0300

    Add interceptors at api file

--- src/api.ts
+++ src/api.ts
@@ -2,4 +2,37 @@ import axios from 'axios';
 
 export const mwu_api = axios.create({
   baseURL: import.meta.env.VITE_MWU_API_ENDPOINT,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+
+mwu_api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('@MWU:token');
+
+    if (token) {
+
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+
+mwu_api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('@MWU:token');
+      localStorage.removeItem('@MWU:user');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
