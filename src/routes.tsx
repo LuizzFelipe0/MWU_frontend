5b45cbb04bf2ed882c051e4cc4968914cc11474a
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-21 18:02:38 -0300

    Add PrivateRoute

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import CategoryTypesPage from './pages/CategoryTypes';
 import Layout from './components/Layout';
 import NotFoundPage from './pages/NotFound';
 import HomePage from './pages/Home';
+import { useAuth } from './hooks/useAuth.tsx';
+import LoginPage from './pages/Login';
+
+const PrivateRoute = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) return null;
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
 
 const MWURoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="category/types" element={<CategoryTypesPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="category/types" element={<CategoryTypesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
       </Route>
     </Routes>
   );
