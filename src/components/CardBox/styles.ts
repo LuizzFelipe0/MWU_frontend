325c8f1d87bc1b2bcdecc506f2ca808b55bd4af3
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-25 23:18:42 -0300

    Remove space-around on header

--- src/components/CardBox/styles.ts
+++ src/components/CardBox/styles.ts
@@ -1,24 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background: white;
+  background: #fff;
   border-radius: 12px; 
   padding: 1.5rem;
 
   /* Responsividade */
   max-width: 50rem;
-
+  
   box-shadow: 0 0.25rem 0.375rem rgba(0, 0, 0, 0.05);
   border: 0.0625rem solid #e5e5ea;
-
-  /* Garante que o conteúdo interno não quebre o layout */
+    
   box-sizing: border-box;
 `;
 
 export const CardHeader = styled.header`
   margin-bottom: 1.875rem; /* 30px */
   display: flex;
-  justify-content: space-around;
   align-items: center;
 
   h1 {
