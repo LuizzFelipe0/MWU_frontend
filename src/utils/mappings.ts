50123927dfd82a619333a0d6e834e65474db2c92
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-03-02 22:09:16 -0300

    Create mappings file

---
+++ src/utils/mappings.ts
@@ -0,0 +1,13 @@
+export const recurrence_intervals = [
+  { value: 'WEEKLY', label: 'Semanal' },
+  { value: 'MONTHLY', label: 'Mensal' },
+  { value: 'YEARLY', label: 'Anual' },
+];
+
+export const account_type_options = [
+  { value: 'Corrente', label: 'Conta Corrente' },
+  { value: 'Poupança', label: 'Conta Poupança' },
+  { value: 'Digital', label: 'Conta Digital' },
+  { value: 'Salário', label: 'Conta Salário' },
+  { value: 'Investimento', label: 'Conta de Investimentos' },
+];
