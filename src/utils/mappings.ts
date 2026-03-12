7b56ff13c7aefcac442c9d46f1b2b4d1cdda0c36
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-03-12 20:34:19 -0300

    Add month_options

--- src/utils/mappings.ts
+++ src/utils/mappings.ts
@@ -11,3 +11,18 @@ export const account_type_options = [
   { value: 'Salário', label: 'Conta Salário' },
   { value: 'Investimento', label: 'Conta de Investimentos' },
 ];
+
+export const month_options = [
+  { value: '01', label: 'Janeiro' },
+  { value: '02', label: 'Fevereiro' },
+  { value: '03', label: 'Março' },
+  { value: '04', label: 'Abril' },
+  { value: '05', label: 'Maio' },
+  { value: '06', label: 'Junho' },
+  { value: '07', label: 'Julho' },
+  { value: '08', label: 'Agosto' },
+  { value: '09', label: 'Setembro' },
+  { value: '10', label: 'Outubro' },
+  { value: '11', label: 'Novembro' },
+  { value: '12', label: 'Dezembro' },
+];
