02433eda9fa4d01a9e7dc610920bdd379dcfc113
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-03-22 22:34:17 -0300

    Add recurrence_id and end_date at transactionTypes

--- src/types/transactionTypes.ts
+++ src/types/transactionTypes.ts
@@ -3,13 +3,14 @@ export interface Transaction {
   user_id: string;
   account_id: string;
   category_id: string;
+  recurrence_id: string;
   name: string;
   description: string;
   amount: number;
   date: string;
-  is_recurring: boolean;
   recurrence_interval: string;
   next_due_date: string;
+  end_date: string;
   created_at: string;
   updated_at: string;
   deleted_at: string | null;
@@ -26,4 +27,5 @@ export interface TransactionInput {
   is_recurring: boolean;
   recurrence_interval?: string;
   next_due_date?: string;
+  end_date?: string;
 }
